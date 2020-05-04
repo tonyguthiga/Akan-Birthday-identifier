4857dcabddce720d3f799cc21eb599435f957305
antony-guthiga <toniguthiga93@gmail.com>
2020-05-04 17:40:38 +0300

    changed formula for getting Akan name

--- js/script.js
+++ js/script.js
@@ -16,48 +16,15 @@ function getAkanName(){
      alert("Please enter valid month") 
   }
 
-  var newDate = new Date(day + "-" + month + "-" + year);
-  var d = newDate.getDay()
+  var a = Math.floor((14 - month) / 12)
+  var y = year - a
+  var m = month + 12 * a - 2
+  var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(year / 400) + Math.floor((31 * m )/ 12)) % 7;
 
   if(male.checked == true){
-     alert("You were born on " + daysOfTheWeek[0] + " and your Akan name would be: " + maleNames[0]);
-  }
-  else if(male.checked == true){
-    alert("You were born on " + daysOfTheWeek[1] + " and your Akan name would be: " + maleNames[1]);
- }
- else if(male.checked == true){
-    alert("You were born on " + daysOfTheWeek[2] + " and your Akan name would be: " + maleNames[2]);
- }
- else if(male.checked == true){
-    alert("You were born on " + daysOfTheWeek[3] + " and your Akan name would be: " + maleNames[3]);
- }
- else if(male.checked == true){
-    alert("You were born on " + daysOfTheWeek[4] + " and your Akan name would be: " + maleNames[4]);
- }
- else if(male.checked == true){
-    alert("You were born on " + daysOfTheWeek[5] + " and your Akan name would be: " + maleNames[5]);
- }
- else if(male.checked == true){
-    alert("You were born on " + daysOfTheWeek[6] + " and your Akan name would be: " + maleNames[6]);
- }
- if(female.checked == true){
-    alert("You were born on " + daysOfTheWeek[0] + " and your Akan name would be: " + femaleNames[0]);
+     alert("You were born on " + daysOfTheWeek[d] + " and your Akan name would be: " + maleNames[d]);
   }
   else if(female.checked == true){
-    alert("You were born on " + daysOfTheWeek[1] + " and your Akan name would be: " + femaleNames[1]);
-  }
-  else if(female.checked == true){
-    alert("You were born on " + daysOfTheWeek[2] + " and your Akan name would be: " + femaleNames[2]);
-  }
-  else if(female.checked == true){
-    alert("You were born on " + daysOfTheWeek[3] + " and your Akan name would be: " + femaleNames[3]);
-  }
-  else if(female.checked == true){
-    alert("You were born on " + daysOfTheWeek[4] + " and your Akan name would be: " + femaleNames[4]);
-  }
-  else if(female.checked == true){
-    alert("You were born on " + daysOfTheWeek[5] + " and your Akan name would be: " + femaleNames[5]);
-  } else if(female.checked == true){
-    alert("You were born on " + daysOfTheWeek[6] + " and your Akan name would be: " + femaleNames[6]);
+    alert("You were born on " + daysOfTheWeek[d] + " and your Akan name would be: " + femaleNames[d]);
   }
 }
